967af3ba36994ed87ef298c9bb6eea507a277c39
Kleber Pinel <kleberpinel@gmail.com>
2011-09-29 14:40:56 -0300

    testing heroku redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
-ENV["REDISTOGO_URL"] ||= "redis://kleberpinel:soumaisdeus@wordlistgenerator.heroku.com:6789"
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:725ba233d7be72f5b745300e4a7723e3@filefish.redistogo.com:9918/"
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
