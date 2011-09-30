243c8cf44f39281a58f610daa7b3492a43b74284
Kleber Pinel <kleberpinel@gmail.com>
2011-09-30 09:31:31 -0300

    test heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,7 @@
-ENV["REDISTOGO_URL"] ||= "redis://redistogo:182d3a3a09cad1306ec76d760ea2d6e7@goosefish.redistogo.com:9971/"
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:59455c85bb6b5f9cd5d52bed2e2d1fc2@bass.redistogo.com:9095/"
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
 #Dir["/srv/redistogo/app/releases/20110926021225/app/jobs/*.rb"].each { |file| require file }
-Dir["/home/redis/9934/app/jobs/*.rb"].each { |file| require file }
+Dir["/home/redis/9095/app/jobs/*.rb"].each { |file| require file }
